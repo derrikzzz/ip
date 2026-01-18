c62e1dca3af54a9e9172e7cb81f64e0fc66f4ca1
Derrick Low <lowderrickym@gmail.com>
2026-01-18 16:17:55 +0800

    added task class

---
+++ src/main/java/Rickshaw/task/Task.java
@@ -0,0 +1,15 @@
+package Rickshaw.task;
+
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false; 
+    }
+
+
+
+
+}
