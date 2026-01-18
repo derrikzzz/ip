beb907eca65810e97d66fd1c44b596602645da76
Derrick Low <lowderrickym@gmail.com>
2026-01-18 16:32:04 +0800

    added tasklist class to store tasks

---
+++ src/main/java/Rickshaw/TaskList.java
@@ -0,0 +1,25 @@
+package Rickshaw;
+
+import Rickshaw.task.Task;
+
+import java.util.ArrayList;
+
+public class TaskList {
+    protected ArrayList<Task> tasks;
+
+    public TaskList() {
+        this.tasks = new ArrayList<Task>();
+    }
+
+    public TaskList(ArrayList<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public void addTask(Task newTask) {
+        this.tasks.add(newTask);
+    }
+
+    public ArrayList<Task> getTasks() {
+        return this.tasks;
+    }
+}
