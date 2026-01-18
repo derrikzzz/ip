477e71b3429e37f9cf1a298db489833db84d8a44
Derrick Low <lowderrickym@gmail.com>
2026-01-18 23:00:28 +0800

    implemented deadline and event classes

---
+++ src/main/java/Rickshaw/task/Event.java
@@ -0,0 +1,20 @@
+package Rickshaw.task;
+
+public class Event extends Task{
+    protected String from;
+    protected String to;
+    public Event(String description) {
+        super(description);
+    }
+
+    public Event(String description, String fromTime, String toTime) {
+        super(description);
+        this.from = fromTime;
+        this.to = toTime;
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
+    }
+}
