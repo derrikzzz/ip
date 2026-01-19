7593eeaf6766dae1dc1d829b5a232bf639a7dad6
Derrick Low <lowderrickym@gmail.com>
2026-01-19 15:21:24 +0800

    implemented parsing logic for todo, deadline and event commands

--- src/main/java/Rickshaw/Parser.java
+++ src/main/java/Rickshaw/Parser.java
@@ -13,6 +13,15 @@ public class Parser {
             case "UNMARK" -> {
                 return new Command(CommandType.UNMARK, new String[]{parts[1]});
             }
+            case "TODO" -> {
+                return parseTodo(trimmedCommand);
+            }
+            case "DEADLINE" -> {
+                return parseDeadline(trimmedCommand);
+            }
+            case "EVENT" -> {
+                return parseEvent(trimmedCommand);
+            }
             case "BYE" -> {
                 return new Command(CommandType.BYE);
             }
@@ -20,8 +29,50 @@ public class Parser {
                 return new Command(CommandType.LIST);
             }
             default -> {
-                return new Command(CommandType.ECHO, new String[]{trimmedCommand});
+                throw new RickshawException("I do not understand this command...");
             }
         }
     }
+
+    private Command parseTodo(String trimmedCommand) throws RickshawException {
+        String description = trimmedCommand.substring(4).trim();
+        if (description.isEmpty()) {
+            throw new RickshawException("Description of todo cannot be empty! ");
+        }
+        return new Command(CommandType.TODO, new String[]{description});
+    }
+
+    private Command parseDeadline(String trimmedCommand) throws RickshawException {
+        // Example of input: "deadline return book /by Sunday"
+        String payload = trimmedCommand.substring(8).trim();
+        String[] segments = payload.split(" /by ");
+        if (segments.length < 2) {
+            throw new RickshawException("Invalid deadline format. Usage: deadline <description> /by <time>");
+        }
+        String description = segments[0];
+        String doneBy = segments[1];
+        return new Command(CommandType.DEADLINE, new String[]{description, doneBy});
+    }
+
+    private Command parseEvent(String trimmedCommand) throws RickshawException {
+        String payload = trimmedCommand.substring(5).trim();
+        String[] fromSplit = payload.split(" /from ");
+        if (fromSplit.length < 2) {
+            throw new RickshawException("Invalid event format. Missing '/from'. Usage: event <description> /from <start]> /to <end>");
+        }
+
+        String description = fromSplit[0];
+        String timeInfo = fromSplit[1];
+
+        String[] timeSplit = timeInfo.split(" /to ");
+        if (timeSplit.length < 2) {
+            throw new RickshawException("Invalid event format. Missing '/to'. Usage: event <description> /from <start> /to <end>");
+        }
+        String from = timeSplit[0];
+        String to = timeSplit[1];
+
+        return new Command(CommandType.EVENT, new String[]{description, from, to});
+    }
+
+
 }
