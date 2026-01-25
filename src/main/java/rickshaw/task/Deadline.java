5a6a4e06e3415fafc2bb3e4b25f38aa10cba9945
Derrick Low <lowderrickym@gmail.com>
2026-01-25 16:13:33 +0800

    store dates as java.time.LocalDateTime in task objects

--- src/main/java/rickshaw/task/Deadline.java
+++ src/main/java/rickshaw/task/Deadline.java
@@ -1,23 +1,26 @@
 package rickshaw.task;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
-    protected String by;
+    protected LocalDateTime by;
+    private static final DateTimeFormatter INPUT_FORMATTER = DateTimeFormatter.ofPattern("d/M/yyyy HHmm");
+    private static final DateTimeFormatter OUTPUT_FORMATTER = DateTimeFormatter.ofPattern("MMM dd yyyy, h:mm a");
 
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        this.by = LocalDateTime.parse(by, INPUT_FORMATTER);
     }
-
-
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return String.format("[D]%s (by: %s)", super.toString(), by.format(OUTPUT_FORMATTER));
     }
 
     @Override
     public String toFileFormat() {
-        return "D | " + (isDone ? "1" : "0") + " | " + description + " | " + by;
+        return String.format("D | %s | %s | %s", (isDone ? "1" : "0"), description, by.format(OUTPUT_FORMATTER));
     }
 
 }
