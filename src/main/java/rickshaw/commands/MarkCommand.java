c8e4d5290e13be76a15ac1feacefee020a28466a
Derrick Low <lowderrickym@gmail.com>
2026-01-25 20:45:49 +0800

    create concrete mark command class

---
+++ src/main/java/rickshaw/commands/MarkCommand.java
@@ -0,0 +1,22 @@
+package rickshaw.commands;
+
+import rickshaw.Ui;
+import rickshaw.Storage;
+import rickshaw.TaskList;
+import rickshaw.task.Task;
+
+public class MarkCommand extends Command {
+
+  private final int taskIndex;
+
+  public MarkCommand(int taskIndex) {
+    this.taskIndex = taskIndex;
+  }
+
+  @Override
+  public void run(TaskList tasks, Ui ui, Storage storage) {
+    Task markedtask = tasks.markTask(taskIndex);
+    ui.showMarkedTask(markedtask);
+    saveTasks(tasks, storage, ui);
+  }
+}
