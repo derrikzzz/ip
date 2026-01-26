c5e1900861e755b9751c06c3a1ebbb4a595fd668
Derrick Low <lowderrickym@gmail.com>
2026-01-26 18:03:56 +0800

    add JavaDoc comments to methods in RickshawException class

--- src/main/java/rickshaw/RickshawException.java
+++ src/main/java/rickshaw/RickshawException.java
@@ -1,6 +1,10 @@
 package rickshaw;
 
 public class RickshawException extends Exception{
+    /**
+     * Constructs a new RickshawException with the specified message.
+     * @param message The message to be displayed when exception is thrown
+     */
     public RickshawException(String message) {
         super(message);
     }
