a85148ef31159fe4d2ac91d7ac6c66c7649d32a9
Derrick Low <lowderrickym@gmail.com>
2026-01-26 18:13:28 +0800

    add JavaDoc comments to methods in EventCommand class

--- src/main/java/rickshaw/commands/EventCommand.java
+++ src/main/java/rickshaw/commands/EventCommand.java
@@ -11,12 +11,24 @@ public class EventCommand extends Command {
   private final String from;
   private final String to;
 
+  /**
+   * Constructs a new EventCommand with given description, from and to
+   * @param description The description of the event task
+   * @param from The start date and time of the event task
+   * @param to The end date and time of the event task
+   */
   public EventCommand(String description, String from, String to) {
     this.description = description;
     this.from = from;
     this.to = to;
   }
 
+  /**
+   * Adds a new event task to the task list, display task added message and save tasks to storage
+   * @param tasks The task list
+   * @param ui The UI component
+   * @param storage The storage component
+   */
   @Override
   public void run(TaskList tasks, Ui ui, Storage storage) {
     Task newEvent = new Event(description, from, to);
