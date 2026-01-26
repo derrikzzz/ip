b0ab19d8d10302292b424c5719c100f150ee3428
Derrick Low <lowderrickym@gmail.com>
2026-01-26 18:14:33 +0800

    add JavaDoc comments to methods in UnmarkCommand class

--- src/main/java/rickshaw/commands/UnmarkCommand.java
+++ src/main/java/rickshaw/commands/UnmarkCommand.java
@@ -8,10 +8,20 @@ import rickshaw.task.Task;
 public class UnmarkCommand extends Command {
   private final int taskIndex;
 
+  /**
+   * Constructs a new UnmarkCommand with the given task index
+   * @param taskIndex
+   */
   public UnmarkCommand(int taskIndex) {
     this.taskIndex = taskIndex;
   }
 
+  /**
+   * Marks task as undone and display unmarked task message and save tasks to storage
+   * @param tasks The task list
+   * @param ui The UI component
+   * @param storage The storage component
+   */
   @Override
   public void run(TaskList tasks, Ui ui, Storage storage) {
     Task unmarkedtask = tasks.unmarkTask(taskIndex);
