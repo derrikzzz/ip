dae7e1237a7226b0b683807ce93d959829f85e9b
Derrick Low <lowderrickym@gmail.com>
2026-01-26 18:12:35 +0800

    add JavaDoc comments to methods in DeadlineCommand class

--- src/main/java/rickshaw/commands/DeadlineCommand.java
+++ src/main/java/rickshaw/commands/DeadlineCommand.java
@@ -10,11 +10,22 @@ public class DeadlineCommand extends Command {
   private final String description;
   private final String by;
 
+  /**
+   * Constructs a new DeadlineCommand with the given description and by
+   * @param description The description of the deadline task
+   * @param by The deadline date and time of the deadline task
+   */
   public DeadlineCommand(String description, String by) {
     this.description = description;
     this.by = by;
   }
 
+  /**
+   * Adds a new deadline task to the task list, display task added message and save tasks to storage
+   * @param tasks The task list
+   * @param ui The UI component
+   * @param storage The storage component
+   */
   @Override
   public void run(TaskList tasks, Ui ui, Storage storage) {
     Task newDeadline = new Deadline(description, by);
