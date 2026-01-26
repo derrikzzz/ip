13678af12b75bbabaeaca913960027298ac48589
Derrick Low <lowderrickym@gmail.com>
2026-01-26 18:13:48 +0800

    add JavaDoc comments to methods in ListCommand class

--- src/main/java/rickshaw/commands/ListCommand.java
+++ src/main/java/rickshaw/commands/ListCommand.java
@@ -5,6 +5,12 @@ import rickshaw.Storage;
 import rickshaw.TaskList;
 
 public class ListCommand extends Command {
+  /**
+   * Displays the list of tasks
+   * @param tasks The task list
+   * @param ui The UI component
+   * @param storage The storage component
+   */
   @Override
   public void run(TaskList tasks, Ui ui, Storage storage) {
     ui.showList(tasks.getTasks());
