3d2672a27fe9345e80599e1bb44e80b18a30f009
Derrick Low <lowderrickym@gmail.com>
2026-01-26 18:14:54 +0800

    add JavaDoc comments to methods in MarkCommand class

--- src/main/java/rickshaw/commands/MarkCommand.java
+++ src/main/java/rickshaw/commands/MarkCommand.java
@@ -9,10 +9,20 @@ public class MarkCommand extends Command {
 
   private final int taskIndex;
 
+  /**
+   * Constructs a new MarkCommand with the given task index
+   * @param taskIndex
+   */
   public MarkCommand(int taskIndex) {
     this.taskIndex = taskIndex;
   }
 
+  /**
+   * Marks task as done and display marked task message and save tasks to storage
+   * @param tasks The task list
+   * @param ui The UI component
+   * @param storage The storage component
+   */
   @Override
   public void run(TaskList tasks, Ui ui, Storage storage) {
     Task markedtask = tasks.markTask(taskIndex);
