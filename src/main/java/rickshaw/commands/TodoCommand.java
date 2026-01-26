a910ca4d6accf47f90cc49cea20cb8e14acc7e87
Derrick Low <lowderrickym@gmail.com>
2026-01-26 18:16:07 +0800

    add JavaDoc comments to methods in TodoCommand class

--- src/main/java/rickshaw/commands/TodoCommand.java
+++ src/main/java/rickshaw/commands/TodoCommand.java
@@ -9,10 +9,20 @@ import rickshaw.task.Todo;
 public class TodoCommand extends Command {
   private final String description;
 
+  /**
+   * Constructs a new TodoCommand with given description
+   * @param description The description of the todo task
+   */
   public TodoCommand(String description) {
     this.description = description;
   }
 
+  /**
+   * Adds a new todo task to the task list, display task added message and save tasks to storage
+   * @param tasks The task list
+   * @param ui The UI component
+   * @param storage The storage component
+   */
   @Override
   public void run(TaskList tasks, Ui ui, Storage storage) {
     Task newTodo = new Todo(description);
