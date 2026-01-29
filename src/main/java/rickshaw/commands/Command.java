cfddbdef955b9ec232645faec8fdaa1313ff5938
Derrick Low <lowderrickym@gmail.com>
2026-01-29 19:08:22 +0800

    format JavaDoc comments in command class

--- src/main/java/rickshaw/commands/Command.java
+++ src/main/java/rickshaw/commands/Command.java
@@ -1,9 +1,10 @@
 package rickshaw.commands;
 
+import java.io.IOException;
+
 import rickshaw.Storage;
 import rickshaw.TaskList;
 import rickshaw.Ui;
-import java.io.IOException;
 
 /**
  * Represents an executable command in the Rickshaw application.
@@ -15,17 +16,17 @@ public abstract class Command {
      * Concrete implementation delegated to subclasses.
      *
      * @param tasks   The task list to execute the command on.
-     * @param ui      The UI component for displaying messages / error messages
+     * @param ui      The UI component for displaying messages / error messages.
      * @param storage The storage component.
      */
-    public abstract void run (TaskList tasks, Ui ui, Storage storage);
+    public abstract void run(TaskList tasks, Ui ui, Storage storage);
 
     /**
      * Saves the current task list to storage. (in data/rickshaw.txt)
      *
      * @param tasks   The task list to save.
      * @param storage The storage component.
-     * @param ui      The UI component for displaying messages / error messages
+     * @param ui      The UI component for displaying messages / error messages.
      */
     protected void saveTasks(TaskList tasks, Storage storage, Ui ui) {
         try {
@@ -34,5 +35,4 @@ public abstract class Command {
             ui.showErrorMessage("Error saving tasks: " + e.getMessage());
         }
     }
-
 }
