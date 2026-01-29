50a3138f58cff8b6ee01dfd190ef06657a066a7d
Derrick Low <lowderrickym@gmail.com>
2026-01-29 19:09:10 +0800

    fix indentation to use 4 spaces in TastListTest class

--- src/test/java/rickshaw/TaskListTest.java
+++ src/test/java/rickshaw/TaskListTest.java
@@ -12,23 +12,23 @@ import rickshaw.Ui;
 import rickshaw.Storage;
 
 public class TaskListTest {
-  private TaskList taskList;
-  private Ui ui;
-  private Storage storage;
+    private TaskList taskList;
+    private Ui ui;
+    private Storage storage;
 
-  @BeforeEach
-  public void setUp() {
-    taskList = new TaskList();
-    ui = new Ui("TestBot");
-    storage = new Storage("data/test.txt");
-  }
+    @BeforeEach
+    public void setUp() {
+        taskList = new TaskList();
+        ui = new Ui("TestBot");
+        storage = new Storage("data/test.txt");
+    }
 
-  @Test
-  public void addTodo_validTodoInput_success() {
-    TodoCommand todo = new TodoCommand("go to the library");
-    todo.run(taskList, ui, storage);
-    assertEquals(1, taskList.size());
-    assertEquals("go to the library", taskList.getTask(0).getDescription());
-    assertEquals(false, taskList.getTask(0).isDone());
-  }
+    @Test
+    public void addTodo_validTodoInput_success() {
+        TodoCommand todo = new TodoCommand("go to the library");
+        todo.run(taskList, ui, storage);
+        assertEquals(1, taskList.size());
+        assertEquals("go to the library", taskList.getTask(0).getDescription());
+        assertEquals(false, taskList.getTask(0).isDone());
+    }
 }
