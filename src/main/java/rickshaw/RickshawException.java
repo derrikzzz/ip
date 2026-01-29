39a89734d8a3f57afa3051bf43ca0df9ba0b459a
Derrick Low <lowderrickym@gmail.com>
2026-01-29 19:04:25 +0800

    format JavaDoc comments in rickshawException class

--- src/main/java/rickshaw/RickshawException.java
+++ src/main/java/rickshaw/RickshawException.java
@@ -1,9 +1,13 @@
 package rickshaw;
 
-public class RickshawException extends Exception{
+/**
+ * Represents exceptions specific to the Rickshaw application.
+ */
+public class RickshawException extends Exception {
     /**
      * Constructs a new RickshawException with the specified message.
-     * @param message The message to be displayed when exception is thrown
+     *
+     * @param message The message to be displayed when exception is thrown.
      */
     public RickshawException(String message) {
         super(message);
