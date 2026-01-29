0f937d6cf188790c95258ffa05c5582228e95587
Derrick Low <lowderrickym@gmail.com>
2026-01-29 19:10:33 +0800

    format JavaDoc comments in task class

--- src/main/java/rickshaw/task/Task.java
+++ src/main/java/rickshaw/task/Task.java
@@ -1,30 +1,59 @@
 package rickshaw.task;
 
-abstract public class Task {
+/**
+ * Represents an abstract task with a description and completion status.
+ */
+public abstract class Task {
     protected String description;
     protected boolean isDone;
 
+    /**
+     * Constructs a Task with the specified description.
+     *
+     * @param description The description of the task.
+     */
     public Task(String description) {
         this.description = description;
-        this.isDone = false; 
+        this.isDone = false;
     }
 
+    /**
+     * Marks the task as done.
+     */
     public void markDone() {
         this.isDone = true;
     }
 
+    /**
+     * Marks the task as not done.
+     */
     public void markUndone() {
         this.isDone = false;
     }
 
+    /**
+     * Gets the status icon of the task.
+     *
+     * @return "X" if done, empty string otherwise.
+     */
     public String getIcon() {
         return this.isDone ? "X" : "";
     }
 
+    /**
+     * Gets the description of the task.
+     *
+     * @return The description.
+     */
     public String getDescription() {
         return this.description;
     }
 
+    /**
+     * Checks if the task is done.
+     *
+     * @return True if done, false otherwise.
+     */
     public boolean isDone() {
         return this.isDone;
     }
@@ -34,6 +63,10 @@ abstract public class Task {
         return String.format("[%s] %s", this.getIcon(), this.description);
     }
 
+    /**
+     * Converts the task to a file format string.
+     *
+     * @return The file format string.
+     */
     public abstract String toFileFormat();
-
 }
