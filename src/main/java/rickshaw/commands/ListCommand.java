2c76c8a9d0dbbb5f32af768ed5f0d527b4983487
Derrick Low <lowderrickym@gmail.com>
2026-01-29 19:11:23 +0800

    format JavaDoc comments in list command class

--- src/main/java/rickshaw/commands/ListCommand.java
+++ src/main/java/rickshaw/commands/ListCommand.java
@@ -1,18 +1,22 @@
 package rickshaw.commands;
 
-import rickshaw.Ui;
 import rickshaw.Storage;
 import rickshaw.TaskList;
+import rickshaw.Ui;
 
+/**
+ * Represents a command to list all tasks.
+ */
 public class ListCommand extends Command {
-  /**
-   * Displays the list of tasks
-   * @param tasks The task list
-   * @param ui The UI component
-   * @param storage The storage component
-   */
-  @Override
-  public void run(TaskList tasks, Ui ui, Storage storage) {
-    ui.showList(tasks.getTasks());
-  }
+    /**
+     * Displays the list of tasks.
+     *
+     * @param tasks The task list.
+     * @param ui The UI component.
+     * @param storage The storage component.
+     */
+    @Override
+    public void run(TaskList tasks, Ui ui, Storage storage) {
+        ui.showList(tasks.getTasks());
+    }
 }
