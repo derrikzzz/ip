7c19dee0e117f46803d3e6263774a4f810083563
Derrick Low <lowderrickym@gmail.com>
2026-01-29 19:09:34 +0800

    fix indentation to use 4 spaces in ParserTest class

--- src/test/java/rickshaw/ParserTest.java
+++ src/test/java/rickshaw/ParserTest.java
@@ -10,30 +10,30 @@ import rickshaw.commands.DeadlineCommand;
 
 public class ParserTest {
 
-  @Test
-  public void parseDeadline_validInput_success() throws RickshawException {
-    Parser parser = new Parser();
-    String input = "deadline return book /by Tuesday";
+    @Test
+    public void parseDeadline_validInput_success() throws RickshawException {
+        Parser parser = new Parser();
+        String input = "deadline return book /by Tuesday";
 
-    Command result = parser.parseDeadline(input);
+        Command result = parser.parseDeadline(input);
 
-    if (result instanceof DeadlineCommand) {
-      DeadlineCommand deadline = (DeadlineCommand) result;
-      assertEquals("return book", deadline.getDescription());
-      assertEquals("Tuesday", deadline.getBy());
+        if (result instanceof DeadlineCommand) {
+            DeadlineCommand deadline = (DeadlineCommand) result;
+            assertEquals("return book", deadline.getDescription());
+            assertEquals("Tuesday", deadline.getBy());
+        }
     }
-  }
 
-  @Test
-  public void parseDeadline_missingTime_throwsException() {
-    Parser parser = new Parser();
-    String input = "deadline return book";
+    @Test
+    public void parseDeadline_missingTime_throwsException() {
+        Parser parser = new Parser();
+        String input = "deadline return book";
 
-    try {
-      parser.parseDeadline(input);
-      fail("Expected RickshawException to be thrown");
-    } catch (RickshawException e) {
-      assertEquals("I recognise that you want to add a deadline task, but the format is incorrect. Usage: deadline <description> /by <time>", e.getMessage());
+        try {
+            parser.parseDeadline(input);
+            fail("Expected RickshawException to be thrown");
+        } catch (RickshawException e) {
+            assertEquals("I recognise that you want to add a deadline task, but the format is incorrect. Usage: deadline <description> /by <time>", e.getMessage());
+        }
     }
-  }
 }
