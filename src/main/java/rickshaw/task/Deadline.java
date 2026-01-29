e6e7eb632ddd56b7bc977adbf3c027179147c6cd
Derrick Low <lowderrickym@gmail.com>
2026-01-29 19:11:51 +0800

    format JavaDoc comments in deadline class

--- src/main/java/rickshaw/task/Deadline.java
+++ src/main/java/rickshaw/task/Deadline.java
@@ -3,11 +3,20 @@ package rickshaw.task;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * Represents a deadline task with a due date.
+ */
 public class Deadline extends Task {
-    protected LocalDateTime by;
     private static final DateTimeFormatter INPUT_FORMATTER = DateTimeFormatter.ofPattern("d/MM/yyyy HHmm");
     private static final DateTimeFormatter OUTPUT_FORMATTER = DateTimeFormatter.ofPattern("MMM dd yyyy, h:mm a");
+    protected LocalDateTime by;
 
+    /**
+     * Constructs a Deadline task with the specified description and due date.
+     *
+     * @param description The description of the deadline.
+     * @param by The due date string in format "d/MM/yyyy HHmm".
+     */
     public Deadline(String description, String by) {
         super(description);
         this.by = LocalDateTime.parse(by, INPUT_FORMATTER);
@@ -22,5 +31,4 @@ public class Deadline extends Task {
     public String toFileFormat() {
         return String.format("D | %s | %s | %s", (isDone ? "1" : "0"), description, by.format(INPUT_FORMATTER));
     }
-
 }
