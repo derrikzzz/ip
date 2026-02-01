3c73af0575d9dab61871a5b8a25675bb90270e76
Derrick Low <lowderrickym@gmail.com>
2026-02-01 22:29:47 +0800

    create main window class and method for handling user input

---
+++ src/main/java/rickshaw/MainWindow.java
@@ -0,0 +1,60 @@
+package rickshaw;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
+/**
+ * Controller for the main GUI window.
+ */
+public class MainWindow extends AnchorPane {
+    @FXML
+    private ScrollPane scrollPane;
+    @FXML
+    private VBox dialogContainer;
+    @FXML
+    private TextField userInput;
+    @FXML
+    private Button sendButton;
+
+    private Rickshaw rickshaw;
+
+    private Image userImage;
+    private Image rickshawImage;
+
+    /**
+     * Initializes the main window.
+     */
+    @FXML
+    public void initialize() {
+        scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
+        try {
+            userImage = new Image(this.getClass().getResourceAsStream("/images/user.png"));
+            rickshawImage = new Image(this.getClass().getResourceAsStream("/images/rickshaw.png"));
+        } catch (Exception e) {
+            System.err.println("Warning: Could not load images from /images/ directory");
+        }
+    }
+
+    public void setRickshaw(Rickshaw r) {
+        rickshaw = r;
+    }
+
+    /**
+     * Handles user input by creating dialog boxes for user and bot responses.
+     */
+    @FXML
+    private void handleUserInput() {
+        String input = userInput.getText();
+        String response = rickshaw.getResponse(input);
+        dialogContainer.getChildren().addAll(
+            DialogBox.getUserDialog(input, userImage),
+            DialogBox.getRickshawDialog(response, rickshawImage)
+        );
+        userInput.clear();
+    }
+}
