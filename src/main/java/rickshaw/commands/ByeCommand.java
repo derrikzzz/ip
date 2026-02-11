741a2e81c5aa31f6e19a33b120746cd6436d025d
Derrick Low <lowderrickym@gmail.com>
2026-02-11 21:27:58 +0800

    add JavaDoc comments to public methods

--- src/main/java/rickshaw/commands/ByeCommand.java
+++ src/main/java/rickshaw/commands/ByeCommand.java
@@ -8,11 +8,25 @@ import rickshaw.Ui;
  * Represents a command to exit the application.
  */
 public class ByeCommand extends Command {
+    /**
+     * Displays the exit message to the user.
+     *
+     * @param tasks   The task list.
+     * @param ui      The UI component.
+     * @param storage The storage component.
+     */
     @Override
     public void run(TaskList tasks, Ui ui, Storage storage) {
         ui.showExitMessage();
     }
 
+    /**
+     * Returns the exit message string for the GUI.
+     *
+     * @param tasks   The task list.
+     * @param storage The storage component.
+     * @return The exit message string.
+     */
     @Override
     public String returnStringResponse(TaskList tasks, Storage storage) {
         return "Bye. Hope to see you again soon!";
