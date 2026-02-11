e4d819dda0b9e19d92b907999a0d97f5303d04f2
Derrick Low <lowderrickym@gmail.com>
2026-02-11 21:26:33 +0800

    add JavaDoc comments to public methods

--- src/main/java/rickshaw/commands/DeleteCommand.java
+++ src/main/java/rickshaw/commands/DeleteCommand.java
@@ -20,6 +20,13 @@ public class DeleteCommand extends Command {
         this.taskIndex = taskIndex;
     }
 
+    /**
+     * Deletes a task from the task list, displays deletion message and saves tasks to storage.
+     *
+     * @param tasks   The task list.
+     * @param ui      The UI component.
+     * @param storage The storage component.
+     */
     @Override
     public void run(TaskList tasks, Ui ui, Storage storage) {
         Task deletedTask = tasks.deleteTask(taskIndex);
@@ -27,6 +34,13 @@ public class DeleteCommand extends Command {
         saveTasks(tasks, storage, ui);
     }
 
+    /**
+     * Returns a response string after deleting a task.
+     *
+     * @param tasks   The task list.
+     * @param storage The storage component.
+     * @return The response string confirming deletion.
+     */
     @Override
     public String returnStringResponse(TaskList tasks, Storage storage) {
         Task deletedTask = tasks.deleteTask(taskIndex);
