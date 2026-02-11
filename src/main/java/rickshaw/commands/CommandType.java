6ab8e74d5e740a615c985ca4ac0be72772830b3d
Derrick Low <lowderrickym@gmail.com>
2026-02-11 21:27:00 +0800

    add JavaDoc comments

--- src/main/java/rickshaw/commands/CommandType.java
+++ src/main/java/rickshaw/commands/CommandType.java
@@ -1,18 +1,29 @@
 package rickshaw.commands;
 
 /**
- * Represents the type of command
+ * Represents the type of command.
  */
 public enum CommandType {
+    /** Lists all tasks. */
     LIST,
+    /** Marks a task as done. */
     MARK,
+    /** Marks a task as not done. */
     UNMARK,
+    /** Deletes a task. */
     DELETE,
+    /** Adds a todo task. */
     TODO,
+    /** Adds a deadline task. */
     DEADLINE,
+    /** Adds an event task. */
     EVENT,
+    /** Exits the application. */
     BYE,
+    /** Finds tasks by keyword. */
     FIND,
+    /** Adds a tag to a task. */
     TAG,
+    /** Removes a tag from a task. */
     UNTAG,
 }
